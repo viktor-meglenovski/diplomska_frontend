ab6199dd3c92ab0e83b2e2055013bc58294125b5
Viktor Meglenovski <viktor.meglenovski@gmail.com>
2023-10-21 20:01:05 +0200

    app title changes

--- src/helpers/axios_ml_helper.js
+++ src/helpers/axios_ml_helper.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import {getAuthToken} from "./axios_helper";
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const request_ml = (method, url, data, custom_headers) => {
+
+    if (!custom_headers){
+        custom_headers = {}
+    }
+    if (getAuthToken() !== null && getAuthToken() !== "null") {
+        custom_headers['Authorization'] = `Bearer ${getAuthToken()}`
+    }
     return axios({
         method: method,
         url: `http://localhost:8000${url}`,
